8d48dd601a48d157e96258bd7814d93c2d30b977
Didi Abdillah HMDev <didiabdillah@student.polindra.ac.id>
2020-11-25 16:50:52 +0700

    mengubah sistem get data grafik di dashboard

--- app/Http/Controllers/SuperAdmin/SuperAdminDashboardController.php
+++ app/Http/Controllers/SuperAdmin/SuperAdminDashboardController.php
@@ -36,16 +36,37 @@ class SuperAdminDashboardController extends Controller
 
     public function grafik_data()
     {
-        $data = DB::table('users')
-            ->select(DB::raw('DATE(created_at) AS tanggal, COUNT(*) AS jumlah_harian'))
-            ->where('status_dev', '!=', 'superadmin')
-            ->orWhere('status_dev', NULL)
-            ->groupBy('tanggal')
-            ->orderBy('tanggal', 'ASC')
-            ->orderBy('id', 'DESC')
-            ->limit(7)
-            ->get();
+        // $data = DB::table('users')
+        //     ->select(DB::raw('DATE(created_at) AS tanggal, COUNT(*) AS jumlah_harian'))
+        //     ->where('status_dev', '!=', 'superadmin')
+        //     ->orWhere('status_dev', NULL)
+        //     ->groupBy('tanggal')
+        //     ->orderBy('tanggal', 'ASC')
+        //     ->orderBy('id', 'DESC')
+        //     ->limit(7)
+        //     ->get();
 
-        return json_encode($data, true);
+        $data_graf = [];
+
+
+        for ($i = 0; $i < 7; $i++) {
+
+
+            $data = DB::table('users')
+                ->select(DB::raw('DATE(created_at) AS tanggal, COUNT(*) AS jumlah_harian'))
+                ->where(DB::raw('DATE(created_at)'), DB::raw('SUBDATE(CURDATE(),' . $i . ')'))
+                ->groupBy('tanggal')
+                ->get();
+
+            if ($data->count() > 0) {
+                $data_graf[$i]["tanggal"] = $data[0]->tanggal;
+                $data_graf[$i]["jumlah_harian"] = $data[0]->jumlah_harian;
+            } else {
+                $data_graf[$i]["tanggal"] = date('Y-m-d', strtotime(date('Y-m-d')) - (($i + 0) * 24 * 60 * 60));
+                $data_graf[$i]["jumlah_harian"] = 0;
+            }
+        }
+
+        return json_encode($data_graf, true);
     }
 }
